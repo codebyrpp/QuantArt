01eb8d6940d3ef44533280862b2c646a6b3d5ecc
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2025-12-07 23:30:18 +0530

    refactor: extract loss utility functions into a new module for better organization

--- quantart/losses/vqperceptual.py
+++ quantart/losses/vqperceptual.py
@@ -4,31 +4,7 @@ import torch.nn.functional as F
 
 from quantart.losses.lpips import LPIPS
 from quantart.components.discriminator import NLayerDiscriminator, weights_init
-
-
-class DummyLoss(nn.Module):
-    def __init__(self):
-        super().__init__()
-
-
-def adopt_weight(weight, global_step, threshold=0, value=0.):
-    if global_step < threshold:
-        weight = value
-    return weight
-
-
-def hinge_d_loss(logits_real, logits_fake):
-    loss_real = torch.mean(F.relu(1. - logits_real))
-    loss_fake = torch.mean(F.relu(1. + logits_fake))
-    d_loss = 0.5 * (loss_real + loss_fake)
-    return d_loss
-
-
-def vanilla_d_loss(logits_real, logits_fake):
-    d_loss = 0.5 * (
-        torch.mean(torch.nn.functional.softplus(-logits_real)) +
-        torch.mean(torch.nn.functional.softplus(logits_fake)))
-    return d_loss
+from quantart.losses.utils import adopt_weight, hinge_d_loss, vanilla_d_loss
 
 
 class VQLPIPSWithDiscriminator(nn.Module):
