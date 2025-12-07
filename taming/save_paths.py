100f515bfacdc347619f3c6004bc48f2ee4b3385
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2025-12-07 23:30:18 +0530

    Remove unused lr_scheduler and save_paths modules

--- taming/save_paths.py
+++
@@ -1,8 +0,0 @@
-import os
-
-rootdir = '../datasets/painter-by-numbers/test'
-f = open("../datasets/painter-by-numbers-test.txt", "w")
-for file in sorted(os.listdir(rootdir)):
-    if file.endswith('.jpg') or file.endswith('.png'):
-        f.write(os.path.join(rootdir, file)+'\n')
-f.close()
