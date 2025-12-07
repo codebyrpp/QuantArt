34f663d1812b3e9d118176202e3acd1a1ffffe5a
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2025-12-07 23:30:18 +0530

    feat: add encoder, decoder, discriminator, quantizer, style transfer, and utility components

---
+++ taming/components/blocks.py
@@ -0,0 +1,198 @@
+import torch
+import torch.nn as nn
+import numpy as np
+
+
+def nonlinearity(x):
+    """
+    Apply the Swish nonlinearity to the input.
+    Swish is a smooth, non-monotonic function that is similar to ReLU,
+    but with a smoother gradient.
+    https://en.wikipedia.org/wiki/Swish_function
+    """
+    # swish
+    return x*torch.sigmoid(x)
+
+
+def Normalize(in_channels):
+    """
+    Normalize the input channels using Group Normalization.
+    If the number of channels is less than 32, use 1 group, 
+    which is equivalent to Layer Normalization.
+    """
+    if in_channels < 32:
+        num_groups = 1
+    else:
+        num_groups = 32
+    return torch.nn.GroupNorm(num_groups=num_groups, num_channels=in_channels, eps=1e-6, affine=True)
+
+
+class Upsample(nn.Module):
+    def __init__(self, in_channels, with_conv):
+        super().__init__()
+        self.with_conv = with_conv
+        if self.with_conv:
+            self.conv = torch.nn.Conv2d(in_channels,
+                                        in_channels,
+                                        kernel_size=3,
+                                        stride=1,
+                                        padding=1)
+
+    def forward(self, x):
+        x = torch.nn.functional.interpolate(
+            x, scale_factor=2.0, mode="nearest")
+        if self.with_conv:
+            x = self.conv(x)
+        return x
+
+
+class Downsample(nn.Module):
+    def __init__(self, in_channels, with_conv):
+        super().__init__()
+        self.with_conv = with_conv
+        if self.with_conv:
+            # no asymmetric padding in torch conv, must do it ourselves
+            self.conv = torch.nn.Conv2d(in_channels,
+                                        in_channels,
+                                        kernel_size=3,
+                                        stride=2,
+                                        padding=0)
+
+    def forward(self, x):
+        if self.with_conv:
+            pad = (0, 1, 0, 1)
+            x = torch.nn.functional.pad(x, pad, mode="constant", value=0)
+            x = self.conv(x)
+        else:
+            x = torch.nn.functional.avg_pool2d(x, kernel_size=2, stride=2)
+        return x
+
+
+class ResnetBlock(nn.Module):
+    def __init__(self, *, in_channels, out_channels=None, conv_shortcut=False,
+                 dropout, temb_channels=512):
+        super().__init__()
+        self.in_channels = in_channels
+        out_channels = in_channels if out_channels is None else out_channels
+        self.out_channels = out_channels
+        self.use_conv_shortcut = conv_shortcut
+
+        self.norm1 = Normalize(in_channels)
+        self.conv1 = torch.nn.Conv2d(in_channels,
+                                     out_channels,
+                                     kernel_size=3,
+                                     stride=1,
+                                     padding=1)
+        if temb_channels > 0:
+            self.temb_proj = torch.nn.Linear(temb_channels,
+                                             out_channels)
+        self.norm2 = Normalize(out_channels)
+        self.dropout = torch.nn.Dropout(dropout)
+        self.conv2 = torch.nn.Conv2d(out_channels,
+                                     out_channels,
+                                     kernel_size=3,
+                                     stride=1,
+                                     padding=1)
+        if self.in_channels != self.out_channels:
+            if self.use_conv_shortcut:
+                self.conv_shortcut = torch.nn.Conv2d(in_channels,
+                                                     out_channels,
+                                                     kernel_size=3,
+                                                     stride=1,
+                                                     padding=1)
+            else:
+                self.nin_shortcut = torch.nn.Conv2d(in_channels,
+                                                    out_channels,
+                                                    kernel_size=1,
+                                                    stride=1,
+                                                    padding=0)
+
+    def forward(self, x, temb=None):
+        h = x
+        h = self.norm1(h)
+        h = nonlinearity(h)
+        h = self.conv1(h)
+
+        if temb is not None:
+            h = h + self.temb_proj(nonlinearity(temb))[:, :, None, None]
+
+        h = self.norm2(h)
+        h = nonlinearity(h)
+        h = self.dropout(h)
+        h = self.conv2(h)
+
+        if self.in_channels != self.out_channels:
+            if self.use_conv_shortcut:
+                x = self.conv_shortcut(x)
+            else:
+                x = self.nin_shortcut(x)
+
+        return x+h
+
+
+class AttnBlock(nn.Module):
+    def __init__(self, in_channels, residual=True, attn_resolution=16):
+        super().__init__()
+        self.in_channels = in_channels
+        self.attn_resolution = attn_resolution
+
+        self.norm = Normalize(in_channels)
+        self.q = torch.nn.Conv2d(in_channels,
+                                 in_channels,
+                                 kernel_size=1,
+                                 stride=1,
+                                 padding=0)
+        self.k = torch.nn.Conv2d(in_channels,
+                                 in_channels,
+                                 kernel_size=1,
+                                 stride=1,
+                                 padding=0)
+        self.v = torch.nn.Conv2d(in_channels,
+                                 in_channels,
+                                 kernel_size=1,
+                                 stride=1,
+                                 padding=0)
+        self.proj_out = torch.nn.Conv2d(in_channels,
+                                        in_channels,
+                                        kernel_size=1,
+                                        stride=1,
+                                        padding=0)
+        self.residual = residual
+
+    def forward(self, x, key=None):
+        xb, xc, xh, xw = x.shape
+        x_in = x
+        h_ = x
+        h_ = self.norm(h_)
+        q = self.q(h_)
+
+        if key is not None:
+            key_norm = self.norm(key)
+            k = self.k(key_norm)
+            v = self.v(key_norm)
+        else:
+            k = self.k(h_)
+            v = self.v(h_)
+
+        # compute attention
+        b, c, h, w = q.shape
+        q = q.reshape(b, c, h*w)
+        q = q.permute(0, 2, 1)   # b,hw,c
+        k = k.reshape(b, c, h*w)  # b,c,hw
+        w_ = torch.bmm(q, k)     # b,hw,hw    w[b,i,j]=sum_c q[b,i,c]k[b,c,j]
+        w_ = w_ * (int(c)**(-0.5))
+        w_ = torch.nn.functional.softmax(w_, dim=2)
+
+        # attend to values
+        v = v.reshape(b, c, h*w)
+        w_ = w_.permute(0, 2, 1)   # b,hw,hw (first hw of k, second of q)
+        # b, c,hw (hw of q) h_[b,c,j] = sum_i v[b,c,i] w_[b,i,j]
+        h_ = torch.bmm(v, w_)
+        h_ = h_.reshape(b, c, h, w)
+
+        h_ = self.proj_out(h_)
+
+        if self.residual:
+            return x_in+h_
+        else:
+            return h_
---
+++ taming/components/style_transfer.py
@@ -0,0 +1,46 @@
+import torch
+import torch.nn as nn
+from taming.components.blocks import ResnetBlock, AttnBlock
+
+
+class StyleTransferBlock(nn.Module):
+    def __init__(self, channels, residual=True, use_conv=True, use_selfatt=True):
+        super().__init__()
+        self.use_conv = use_conv
+        self.use_selfatt = use_selfatt
+        if use_conv:
+            self.res_block = ResnetBlock(in_channels=channels,
+                                         out_channels=channels,
+                                         temb_channels=0,
+                                         dropout=0.0)
+        if use_selfatt:
+            self.self_attn_block = AttnBlock(channels, residual=residual)
+        self.attn_block = AttnBlock(channels, residual=residual)
+
+    def forward(self, x, ref):
+        if self.use_conv:
+            x = self.res_block(x)
+        if self.use_selfatt:
+            x = self.self_attn_block(x)
+        if ref is not None:
+            x = self.attn_block(x, ref)
+        return x
+
+
+class StyleTransferModule(nn.Module):
+    def __init__(self, channels, block_num=6, residual=True, use_conv=True, use_selfatt=True):
+        super().__init__()
+        blocks = []
+        for i in range(block_num):
+            blocks.append(StyleTransferBlock(
+                channels, residual=residual, use_conv=use_conv, use_selfatt=use_selfatt))
+        self.blocks = torch.nn.Sequential(*blocks)
+
+    def forward(self, x, ref, return_all=False):
+        x_list = []
+        for block in self.blocks:
+            x = block(x, ref)
+            x_list.append(x)
+        if return_all:
+            return x_list
+        return x
